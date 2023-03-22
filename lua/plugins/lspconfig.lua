f2fd3e054fcff24397827ceb76872cc315861e7a
kjuulh <contact@kjuulh.io>
2023-03-22 11:04:23 +0100

    fix: pylsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,44 @@
 return {
   "neovim/nvim-lspconfig",
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              autopep8 = {
+                enabled = false
+              },
+              yapf = {
+                enabled = false
+              },
+              pylint = {
+                enabled = true
+              },
+              flake8 = {
+                enabled = true
+              },
+              ["pylsp-mypy"] = {
+                enabled = true,
+                livemode = true
+              },
+              ["pylsp-rope"] = {
+                enabled = true
+              },
+              ["pyls-isort"] = {
+                enabled = true
+              },
+              ["python-lsp-black"] = {
+                enabled = true
+              },
+              ["pyls-memestra"] = {
+                enabled = true
+              },
+            }
+          }
+        }
+      }
+    }
+  }
 }
