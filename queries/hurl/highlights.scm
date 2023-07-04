d4090ea19f3747701529091c4dd280410b4ff20c
kjuulh <contact@kjuulh.io>
2023-07-04 20:11:05 +0200

    feat: with response code
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- queries/hurl/highlights.scm
+++ queries/hurl/highlights.scm
@@ -1,17 +1,29 @@
 [
-        "GET"
-        "POST"
-        "PUT"
-        "DELETE"
-        "CONNECT"
-        "OPTIONS"
-        "TRACE"
-        "PATCH"
-        "LINK"
-        "UNLINK"
-        "PURGE"
-        "LOCK"
-        "UNLOCK"
-        "PROPFIND"
-        "VIEW"
+  "GET"
+  "POST"
+  "PUT"
+  "DELETE"
+  "CONNECT"
+  "OPTIONS"
+  "TRACE"
+  "PATCH"
+  "LINK"
+  "UNLINK"
+  "PURGE"
+  "LOCK"
+  "UNLOCK"
+  "PROPFIND"
+  "VIEW"
  ] @keyword
+
+[
+ (url)
+] @string
+
+[
+ (response)
+] @keyword.response
+
+[
+ (status_code_pattern)
+] @constant
